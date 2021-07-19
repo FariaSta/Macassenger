5308d3aa880dcd2c0db02c0fd85455f363629f99
FariaSta <davidfarias30@gmail.com>
2021-07-19 14:51:55 -0400

    header logo added

---
+++ components/MacaLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="logo" src="../public/svg/LogoWhite.svg" alt="Macassenger">
+</template>
+
+<style>
+    .logo
+    {
+        margin-top: 18px;
+        margin-left: 5%;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <maca-logo />
+    <div class="title">
+      <h1>Chat for Friends</h1>
+    </div>
+  </div>
+  
 </template>
+<style scoped>
+  
+  .container
+  {
+    background: #313F49;
+  }
+
+</style>
 
 <script>
-export default {}
+import MacaLogo from '../components/MacaLogo.vue'
+export default {MacaLogo}
 </script>
+
+MacaLogo
