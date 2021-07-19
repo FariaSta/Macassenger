f46e496a646caada069afd12c7593929d6780df7
FariaSta <davidfarias30@gmail.com>
2021-07-19 15:48:59 -0400

    input component added

---
+++ components/InputGeneral.vue
@@ -0,0 +1,20 @@
+<template>
+  <input class="inputGeneral">
+</template>
+
+<style scoped>
+
+  .inputGeneral
+  {
+    width: 314px;
+    height: 36px;
+    border: 2px solid #FFFFFF;
+    border-radius: 10px;
+    background: none;
+    color:white;
+    font-size: 20px;
+    text-align: center;
+  }
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,61 @@
 <template>
-  <div class="container">
+  <div class="containerr">
     <maca-logo />
     <div class="title">
-      <h1>Chat for Friends</h1>
+      <h1>Chat for </br> Friends</h1>
     </div>
+    
+    <form action="/">
+      <input-general type="email" placeholder="Email" />
+      <input-general type="Password" placeholder="Password" />
+      <span>Forgot your password?</span>
+    </form>
+
   </div>
   
 </template>
 <style scoped>
   
-  .container
+  .containerr
   {
     background: #313F49;
+  }
+  .title
+  {
+    width: 295px;
+    height: 153px;
+    margin: 100px 0 67px 9%;
+    display: flex;
+    align-items: center;
+  }
+  .title h1 
+  {
+    color: white;
+    font-size: 55px;
+  }
+  form 
+  {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+  }
+  .inputGeneral
+  {
+    margin-bottom: 11px;
+  }
+  span
+  {
+    color: white;
+    font-size: 13px;
   }
 
 </style>
 
 <script>
 import MacaLogo from '../components/MacaLogo.vue'
-export default {MacaLogo}
+import InputGeneral from '../components/InputGeneral.vue'
+export default {
+  components: {MacaLogo, InputGeneral}
+}
 </script>
-
-MacaLogo
