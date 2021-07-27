6d809ffb4290e4efa34b2466795c20cdfa830faa
FariaSta <davidfarias30@gmail.com>
2021-07-27 12:23:44 -0400

    search component finished

---
+++ components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search">
+    <input  placeholder="Search">
+    <img class="back" src="../public/svg/search.svg" >
+  </div>
+
+  
+</template>
+
+<style scoped>
+
+  input
+  {
+    width: 85%;
+    height: 36px;
+    border: 2px solid #7BC411;
+    border-radius: 10px;
+    background: none;
+    color:white;
+    font-size: 20px;
+    text-align: left;
+    padding: 0 0 0 15px;
+    position: absolute;
+    left: 37px;
+  }
+  img
+  {
+    position: absolute;
+    top: 27px;
+    left: 85%;
+
+  }
+
+
+</style>
--- pages/chats.vue
+++ pages/chats.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        <h1>Hola que tal</h1>
+
+    <div class="chats">
+        <div class="header">
+            <img src="../public/svg/menu.svg" alt="">
+            <search />
+        </div>
     </div>
+
 </template>
+
+<style scoped>
+
+    .chats
+    {
+        background: #313F49;
+    }
+    .header
+    {
+        padding-top: 19px;
+        width: 100%;
+        display: flex;
+    }
+    .header img 
+    {
+        margin-left: 15px;
+        margin-top: 1px;
+    }
+
+</style>
+
+<script>
+import Search from '~/components/Search.vue'
+export default {
+    components: {Search}
+}
+</script>
